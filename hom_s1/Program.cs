e7a1ac7b4fb8cb2bb5f0e3e344139aa31e08f278
Doston <119062575+dosrih@users.noreply.github.com>
2023-01-07 17:33:48 +0500

    Add files via upload

---
+++ hom_s1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int count = Convert.ToInt32(Console.ReadLine());
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count  + 1);
+        Console.Write (count + " ");
+    }
+};
